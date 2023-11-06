42ddee64f00a932da3ad5c984c023574797c8e0c
Lala Arnav Vatsal <lalaarnav003@gmail.com>
2023-11-06 15:22:38 +0530

    Changed response function

--- Frontend/app.py
+++ Frontend/app.py
@@ -11,74 +11,70 @@ st.set_page_config(page_title='UniBot', page_icon='Resources/bot3.png', layout="
                    initial_sidebar_state="collapsed")
 
 
-def generate_response(user_input):
-    url = f"https://general-runtime.voiceflow.com/state/user/{user_id}/interact"
+def generate_response(user_id, request):
+    with st.spinner("Thinking..."):
+        url = f"https://general-runtime.voiceflow.com/state/user/{user_id}/interact"
 
-    payload = {"action": {
-        "type": "text",
-        "payload": f"{user_input}"
-    },
-        "config": {
-            "tts": False,
-            "stripSSML": True,
-            "stopAll": True,
-            "excludeTypes": ["block", "debug", "flow"]
+        # payload = {"action": {
+        #     "type": "text",
+        #     "payload": f"{user_input}"
+        # },
+        #     "config": {
+        #         "tts": False,
+        #         "stripSSML": True,
+        #         "stopAll": True,
+        #         "excludeTypes": ["block", "debug", "flow"]
+        #     }
+        # }
+
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "Authorization": api_key
         }
-    }
 
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "Authorization": api_key
-    }
-
-    # Start a conversation
-    response = requests.post(
-        url=url,
-        json=payload,
-        headers=headers,
-    )
-    try:
-        output = response.json()
-        print(output)
-        message = None
-        for o in output:
-            if o["type"] == 'text':
-                message = o["payload"]["message"]
-        return message
-    except KeyError:
-        return "Error occurred. Can't connect to servers."
+        # Start a conversation
+        response = requests.post(
+            url=url,
+            json={ 'request': request },
+            headers=headers,
+        )
+        try:
+            output = response.json()
+            message = None
+            for trace in output:
+                if trace["type"] == 'text' or trace["type"] == 'speak':
+                    message = trace["payload"]["message"]
+                elif trace['type'] == 'end':
+                    return None
+            return message
+        except KeyError:
+            return "Error occurred. Can't connect to servers."
 
 
 def chat():
     with st.container():
         if st.session_state.messages == []:
-            user_input = f'''Hello!!'''
-            st.session_state.messages = [
-                {
-                    "role": "user",
-                    "content": user_input,
-                },
-            ]
-            # response = generate_response(user_input)
-            response = '''I am an AI assistant for Manipal University Jaipur.
-            Ask me anything!'''
+            response = generate_response(user_id, { 'type': 'launch' })
+            if response == None:
+                response = '''I am an AI assistant for Manipal University Jaipur.
+                Ask me anything!'''
             st.session_state.messages.append({"role": "assistant", "content": response})
             st.rerun()
 
-        for i, message in enumerate(st.session_state.messages[1:]):
+        for i, message in enumerate(st.session_state.messages):
             if message["role"] == "user":
-                with st.chat_message("user"):
+                with st.chat_message(name="user", avatar="./Resources/user.jpeg"):
                     st.write(message["content"])
             elif message["role"] == "assistant":
-                with st.chat_message("assistant"):
+                with st.chat_message(name="assistant", avatar="./Resources/bot2.png"):
                     st.write(message["content"])
 
         user_input = st.chat_input("Enter your query.")
 
         if user_input is not None and user_input.strip() != "":
             st.session_state.messages.append({"role": "user", "content": user_input})
-            response = generate_response(user_input)
+            response = generate_response(user_id, { 'type': 'text', 'payload': user_input })
             st.session_state.messages.append({"role": "assistant", "content": response})
             st.rerun()
 
@@ -93,7 +89,5 @@ def readHTML(htmlFile):
     return html
 
 
-html = readHTML('Frontend/app.html')
-st.markdown(html, unsafe_allow_html=True)
-
+st.markdown(readHTML('Frontend/app.html'), unsafe_allow_html=True)
 chat()
